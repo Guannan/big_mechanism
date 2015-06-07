7df32b030293311ab3f8739b2ed2c82eb6c839bd
Guannan <gren3@jhu.edu>
2015-06-07 14:08:51 -0700

    Work done with learning using scikit-learn library SVM. No table list to filter out pmcids with no tables.

--- doc_filter.py
+++ doc_filter.py
@@ -5,6 +5,8 @@ import collections
 import operator
 import random
 import math
+import numpy as np
+from sklearn import svm
 
 def _complete():
 	"""
@@ -84,7 +86,12 @@ def _parse_bigram(text_list):
 	the columns being the occurrences
 	"""
 	bigrams = collections.defaultdict(int)
+	doc_occurrence = collections.defaultdict(int)  # number of docs word occurs in
+	tf = collections.defaultdict(float)   # weighted frequency of word occurrence in 1 doc
+	total_doc_count = len(text_list)
 
+	counted = False
+	occurrence = collections.defaultdict(int)
 	for text in text_list:
 		words = text.strip().split(' ')
 		for i in xrange(len(words)-1):
@@ -95,8 +102,24 @@ def _parse_bigram(text_list):
 			if word_one != None and word_two != None:
 				bigram = ' '.join([word_one, word_two])
 				bigrams[bigram] += 1
+				occurrence[bigram] += 1
+				if not counted:
+					doc_occurrence[bigram] += 1
+					counted = True
+		for word, count in occurrence.iteritems():
+			tf[word] += float(count)/total_doc_count
+		occurrence = collections.defaultdict(int)
+		counted = False
 
-	return bigrams
+	idf = collections.defaultdict(float)
+	for word, doc_count in doc_occurrence.iteritems():
+		idf[word] = math.log(float(total_doc_count)/doc_count)
+	
+	tf_idf = collections.defaultdict(float)
+	for word in idf:
+		tf_idf[word] += tf[word] * idf[word]		
+
+	return bigrams, tf_idf
 
 def _parse_trigram(text_list):
 	"""
@@ -201,11 +224,20 @@ def _generate_ngrams():
 	for word, count in reversed(sorted_trigrams):
 		fh.write(word + ' ' + str(float(count)/bad_count) + '\n')
 
+def _get_tableless_id():
+	global tableless_id
+
+	tableless_id = []
+	for line in open("no_table_papers.txt", 'r'):
+		tableless_id.append(line.strip())
+
 def _parse_labeled_data():
 	global labeled_data
 	global training_data_label
 	global test_data_label
+	global tableless_id
 
+	_get_tableless_id()
 	random.seed()
 	labeled_data = collections.defaultdict(str)
 	training_data_label = collections.defaultdict(str)
@@ -213,22 +245,90 @@ def _parse_labeled_data():
 
 	for line in open("labeled_preprocessing_data.txt", 'r'):
 		temp = line.strip().split(',')
-		labeled_data[temp[0]] = temp[1]  # match pmc id with labels Y, ?, N
-		num = random.randint(0,99)
-		if num > 33:  # 1 and 2 divide
-			test_data_label[temp[0]] = temp[1]
-		else:
-			training_data_label[temp[0]] = temp[1]
+		if not temp[0] in tableless_id:
+			labeled_data[temp[0]] = temp[1]  # match pmc id with labels Y, ?, N
+			num = random.randint(0,9)
+			if num >= 8:  # ratio of training vs test data
+				test_data_label[temp[0]] = temp[1]
+			else:
+				training_data_label[temp[0]] = temp[1]
+
+def _svm_train():
+	global papers
+	global training_data_label
+	global feature_list
+
+	feature_count = 50
+	word_occurrence = collections.defaultdict(int)
+	log_likelihood = collections.defaultdict(float)
+	training_data = np.zeros((len(training_data_label), feature_count))
+	training_target = np.zeros((len(training_data_label)))
+
+	tf_idf = collections.defaultdict(float)
+	for pmcid, values in papers.iteritems():
+		id_num = pmcid[3:]  # remove 'PMC' tag
+		if training_data_label[id_num]:
+			bigrams, scores = _parse_bigram([values['title']])
+			for word, score in scores.iteritems():
+				tf_idf[word] += score
+			bigrams, scores = _parse_bigram([values['abstract']])
+			for word, score in scores.iteritems():
+				tf_idf[word] += score
+
+	cur_feature_num = 0
+	feature_list = []
+	feature_vec = []
+
+	sorted_tf_idf = sorted(tf_idf.items(), key=operator.itemgetter(1))
+	for word, count in reversed(sorted_tf_idf):
+		cur_feature_num += 1
+		feature_list.append(word)
+		if cur_feature_num >= feature_count:
+			break
+
+	example_count = 0
+	for pmcid, values in papers.iteritems():
+		id_num = pmcid[3:]  # remove 'PMC' tag
+		if training_data_label[id_num]:
+			bigrams, junk = _parse_bigram([values['title']])
+			for word, count in bigrams.iteritems():
+				word_occurrence[word] += count
+			bigrams, junk = _parse_bigram([values['abstract']])			
+			for word, count in bigrams.iteritems():
+				word_occurrence[word] += count
+
+			for feature in feature_list:
+				feature_vec.append(int(word_occurrence[feature]))
+
+			if training_data_label[id_num] == 'Y' or training_data_label[id_num] == '?':
+				training_data[example_count:] = feature_vec
+				training_target[example_count] = 1
+			elif training_data_label[id_num] == 'N':
+				training_data[example_count:] = feature_vec
+				training_target[example_count] = 0
+
+			word_occurrence = collections.defaultdict(int)
+			feature_vec = []
+			example_count += 1
+
+	# scikit learn for SVM train
+	clf = svm.SVC()
+	clf.fit(training_data, training_target)
+	return clf
 
 def _train():
 	global papers
 	global training_data_label
 	global log_likelihood
+	global log_likelihood_negative
 
 	word_occurrence = collections.defaultdict(int)
 	log_likelihood = collections.defaultdict(float)
+	word_occurrence_negative = collections.defaultdict(int)
+	log_likelihood_negative = collections.defaultdict(float)
 
 	good_count = 0
+	bad_count = 0
 
 	tf_idf = collections.defaultdict(float)
 	for pmcid, values in papers.iteritems():
@@ -259,26 +359,80 @@ def _train():
 			for word, count in unigrams.iteritems():
 				word_occurrence[word] += count
 			good_count += 1
+		elif training_data_label[id_num] == 'N':
+			unigrams, junk = _parse_unigram([values['title']])	
+			for word, count in unigrams.iteritems():
+				word_occurrence_negative[word] += count
+			unigrams, junk = _parse_unigram([values['abstract']])			
+			for word, count in unigrams.iteritems():
+				word_occurrence_negative[word] += count
+			bad_count += 1
 
-	feature_count = 20
+	feature_count = 50
 	cur_feature_num = 0
 
 	sorted_tf_idf = sorted(tf_idf.items(), key=operator.itemgetter(1))
 	for word, count in reversed(sorted_tf_idf):
-		if word_occurrence[word] > 0:
-			print word, word_occurrence[word]			
+		if word_occurrence[word] > 0 and word_occurrence_negative[word] > 0:
+			# print word, word_occurrence[word]
 			log_likelihood[word] = math.log(float(word_occurrence[word])/good_count)
+			log_likelihood_negative[word] = math.log(float(word_occurrence_negative[word])/bad_count)
 			cur_feature_num += 1
 		if cur_feature_num >= feature_count:
 			return
 
-	# sorted_word_occurrence = sorted(word_occurrence.items(), key=operator.itemgetter(1))
-	# for word, count in reversed(sorted_word_occurrence):
-	# 	log_likelihood[word] = math.log(float(count)/good_count)
-	# 	cur_feature_num += 1
-	# 	if cur_feature_num >= feature_count:
-	# 		return
-	# 	print word, log_likelihood[word]
+def _svm_test(model):
+	global papers
+	global test_data_label
+	global feature_list
+
+	predictions = collections.defaultdict(str)
+	word_occurrence = collections.defaultdict(int)
+	feature_vec = []
+	for pmcid, values in papers.iteritems():
+		id_num = pmcid[3:]  # remove 'PMC' tag
+		if test_data_label[id_num]:
+			bigrams, junk = _parse_bigram([values['title']])
+			for word, count in bigrams.iteritems():
+				word_occurrence[word] += count
+			bigrams, junk = _parse_bigram([values['abstract']])	
+			for word, count in bigrams.iteritems():
+				word_occurrence[word] += count
+
+			for feature in feature_list:
+				feature_vec.append(int(word_occurrence[feature]))
+
+			predictions[id_num] = model.predict([feature_vec])
+
+		word_occurrence = collections.defaultdict(int)	
+		feature_vec = []
+	
+	tp = 0.0
+	tn = 0.0
+	fp = 0.0
+	fn = 0.0
+	for id_num, prediction in predictions.iteritems():
+		print predictions[id_num][0]
+		if test_data_label[id_num] == 'N' and predictions[id_num][0] == 0:
+			tn += 1.0
+		elif test_data_label[id_num] in ['?','Y'] and predictions[id_num][0] == 1:
+			tp += 1.0
+		elif test_data_label[id_num] in ['?','Y'] and predictions[id_num][0] == 0:
+			fn += 1.0
+		elif test_data_label[id_num] == 'N' and predictions[id_num][0] == 1:
+			fp += 1.0
+		else:
+			raise
+
+	print 'Predictions made : ', len(predictions)
+	print 'True negatives : ', (tn + fp)
+	print 'Accuracy : ', float(tp+tn)/(tp+tn+fp+fn)
+	precision = 0.0 if tp == 0.0 else float(tp)/(tp+fp)
+	print 'Precision : ', precision
+	recall = float(tp)/(tp+fn)
+	print 'Recall : ', recall
+	f1 = 2*float(recall*precision/(recall+precision)) if (recall+precision) > 0 else 0
+	print 'F1 score : ', f1
 
 def _test():
 	global papers
@@ -286,6 +440,7 @@ def _test():
 	global log_likelihood
 
 	ll = 0
+	ll_negative = 0
 	predictions = collections.defaultdict(str)
 	for pmcid, values in papers.iteritems():
 		id_num = pmcid[3:]  # remove 'PMC' tag
@@ -294,15 +449,21 @@ def _test():
 			for word, count in unigrams.iteritems():
 				if log_likelihood[word] != 0.0:
 					ll += log_likelihood[word]   # TODO use count here?
+				if log_likelihood_negative[word] != 0.0:
+					ll_negative += log_likelihood_negative[word]
 			unigrams, junk = _parse_unigram([values['abstract']])	
 			for word, count in unigrams.iteritems():
 				if log_likelihood[word] != 0.0:
 					ll += log_likelihood[word]
-			if ll < -2 or ll == 0.0:
-				predictions[id_num] = 'N'
-			else:
+				if log_likelihood_negative[word] != 0.0:
+					ll_negative += log_likelihood_negative[word]					
+			# if ll < -2 or ll == 0.0:
+			if ll > ll_negative or ll == 0.0 or ll_negative == 0.0:			
 				predictions[id_num] = '?'
+			else:
+				predictions[id_num] = 'N'
 		ll = 0
+		ll_negative = 0
 
 	tp = 0.0
 	tn = 0.0
@@ -310,16 +471,18 @@ def _test():
 	fn = 0.0
 	for id_num, prediction in predictions.iteritems():
 		if test_data_label[id_num] == 'N' and predictions[id_num] == 'N':
-			tp += 1.0
-		elif test_data_label[id_num] in ['?','Y'] and predictions[id_num] == '?':
 			tn += 1.0
+		elif test_data_label[id_num] in ['?','Y'] and predictions[id_num] == '?':
+			tp += 1.0
 		elif test_data_label[id_num] in ['?','Y'] and predictions[id_num] == 'N':
-			fp += 1.0
-		elif test_data_label[id_num] == 'N' and predictions[id_num] == '?':
 			fn += 1.0
+		elif test_data_label[id_num] == 'N' and predictions[id_num] == '?':
+			fp += 1.0
+		else:
+			raise
 
 	print 'Predictions made : ', len(predictions)
-	print 'Test dataset size : ', len(test_data_label)
+	print 'True negatives : ', (tn + fp)
 	print 'Accuracy : ', float(tp+tn)/(tp+tn+fp+fn)
 	precision = float(tp)/(tp+fp)
 	print 'Precision : ', precision
@@ -327,7 +490,6 @@ def _test():
 	print 'Recall : ', recall
 	f1 = 2*float(recall*precision/(recall+precision)) if (recall+precision) > 0 else 0
 	print 'F1 score : ', f1
-	# print 'Length of log : ', len(log_likelihood)
 
 def main(argv):
 	global stop_words
@@ -409,13 +571,15 @@ def main(argv):
 	for line in open("stems.txt", 'r'):
 		stem_patterns.append(line.rstrip())
 
-	_train()
-	_test()
+	# _train()
+	# _test()
+	model = _svm_train()
+	_svm_test(model)
 	# _generate_ngrams()
 
 	print 'Good articles : ', good_count
 	print 'Bad articles : ', bad_count
-	print 'unlabeled_count : ', unlabeled_count
+	print 'Unlabeled/tableless articles : ', unlabeled_count
 
 if __name__ == '__main__': 
 	main(sys.argv)
